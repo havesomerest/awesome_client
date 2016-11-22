d257aed7761c2d45416a3e2c838818d0ea310e44
hevi <hever.istvan@gmail.com>
2016-11-22 21:23:56 +0000

    socket communication

--- src/main/java/org/springframework/shell/samples/helloworld/commands/TcpConnectionService.java
+++ src/main/java/org/springframework/shell/samples/helloworld/commands/TcpConnectionService.java
@@ -31,37 +31,43 @@ public class TcpConnectionService implements CommandMarker {
 
     }
 
-    @CliCommand(value = "get test", help = "Print a simple hello world message")
+    @CliCommand(value = "get request", help = "Print a simple hello world message")
     public String simple(
-            @CliOption(key = { "mapping" }, mandatory = false, help = "The hello world message") final String mapping) {
+            @CliOption(key = { "position" }, mandatory = false, help = "The hello world message") final Integer pos) {
 
+        Integer position = pos;
             String fromServer, fromUser;
         try {
             PrintWriter socketWriter = new PrintWriter(kkSocket.getOutputStream(), true);
             BufferedReader socketReader = new BufferedReader(
                     new InputStreamReader(kkSocket.getInputStream()));
 
-
-
-
             boolean finished = false;
 
-            socketWriter.println("getrequest:" + mapping);
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("command", "LIST_REQUESTS");
+            jsonObject.put("position", position);
+
+            socketWriter.println(jsonObject.toString());
             fromServer = socketReader.readLine();
 
             if (fromServer != null && fromServer.equals("BYE.")) {
                 kkSocket.close();
                 finished = true;
             }
-
-            //socketWriter.println(fromUser);
-
-
         } catch (Exception e) {
-            return "exception: " + e.getMessage();
+            return new JSONObject(e).toString(2);
         }
-        return new JSONObject(fromServer).toString(2);
-        //return "Message = [" + message + "] Location = [" + location + "]";
+        String returnValue = "";
+
+        if (fromServer != null) {
+            JSONObject fromServerObject = new JSONObject(fromServer);
+            returnValue = fromServerObject.toString(2);
+        } else {
+            returnValue = fromServer;
+        }
+
+        return returnValue + "\n";
     }
 
 }
